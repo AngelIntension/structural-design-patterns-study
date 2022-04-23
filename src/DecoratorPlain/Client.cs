3cd69b05ad6b067c440dd752a5e90c91a88f7859
Byron Burks <byron.burks@gmail.com>
2022-04-23 00:37:46 -0500

    add IComponent. add Client

---
+++ src/DecoratorPlain/Client.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace DecoratorPlain
+{
+    public class Client
+    {
+        private IComponent component;
+
+        public Client(IComponent component)
+        {
+            this.component = component;
+        }
+
+        public Task ExecuteAsync(HttpContext context)
+        {
+            var result = component.Operation();
+            return context.Response.WriteAsync($"Operation: {result}");
+        }
+    }
+}
---
+++ test/DecoratorPlainTest/ClientTest.cs
@@ -0,0 +1,33 @@
+﻿using DecoratorPlain;
+using ForEvolve.Testing.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DecoratorPlainTest
+{
+    public class ClientTest
+    {
+        public class ExecuteAsync : ClientTest
+        {
+            private readonly HttpContextHelper httpContextHelper = new HttpContextHelper();
+
+            [Fact]
+            public async Task ShouldResponseWriteTheResultOfTheComponentOperation()
+            {
+                const string testValue = "Test Value";
+                // arrange
+                var componentMock = new Mock<IComponent>();
+                componentMock.Setup(x => x.Operation()).Returns(testValue).Verifiable();
+                var sut = new Client(componentMock.Object);
+
+                // act
+                await sut.ExecuteAsync(httpContextHelper.HttpContextMock.Object);
+
+                // assert
+                httpContextHelper.HttpResponseFake.BodyShouldEqual($"Operation: {testValue}");
+            }
+        }
+    }
+}
