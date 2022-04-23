9d5e679b588d284d5c50be63b0e3da10451d4ca9
Byron Burks <byron.burks@gmail.com>
2022-04-23 16:57:28 -0500

    add ExternalGreeterAdapter

---
+++ src/Adapter/ExternalGreeterAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Adapter
+{
+    public class ExternalGreeterAdapter : IGreeter
+    {
+        private ExternalGreeter adaptee;
+
+        public ExternalGreeterAdapter(ExternalGreeter adaptee)
+        {
+            this.adaptee = adaptee ?? throw new ArgumentNullException(nameof(adaptee));
+        }
+
+        public string Greeting()
+        {
+            return adaptee.GreetByName("System");
+        }
+    }
+}
---
+++ test/AdapterTest/ExternalGreeterAdapterTest.cs
@@ -0,0 +1,39 @@
+﻿using Adapter;
+using System;
+using Xunit;
+
+namespace AdapterTest
+{
+    public class ExternalGreeterAdapterTest
+    {
+        public class ExternalGreeterAdapterShould : ExternalGreeterAdapterTest
+        {
+            [Fact]
+            public void GuardAgainstNullAdaptee()
+            {
+                // act
+                var error = Assert.Throws<ArgumentNullException>(() => new ExternalGreeterAdapter(null));
+
+                // assert
+                Assert.Equal("adaptee", error.ParamName);
+            }
+        }
+
+        public class GreetingShould : ExternalGreeterAdapterTest
+        {
+            [Fact]
+            public void ReturnDesiredGreeting()
+            {
+                // arrange
+                var expected = "Adaptee says: hi System!";
+                IGreeter sut = new ExternalGreeterAdapter(new ExternalGreeter());
+
+                // act
+                var result = sut.Greeting();
+
+                // assert
+                Assert.Equal(expected, result);
+            }
+        }
+    }
+}
