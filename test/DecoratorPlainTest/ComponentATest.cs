62c30906e65b6181ba5285ae7ca3d907fc4c543f
Byron Burks <byron.burks@gmail.com>
2022-04-23 00:54:16 -0500

    add ComponentA. configure Startup services to make use of Client and ComponentA

---
+++ test/DecoratorPlainTest/ComponentATest.cs
@@ -0,0 +1,21 @@
+﻿using DecoratorPlain;
+using Xunit;
+
+namespace DecoratorPlainTest
+{
+    public class ComponentATest
+    {
+        [Fact]
+        public void ShouldReturnHelloFromOperation()
+        {
+            // arrange
+            IComponent sut = new ComponentA();
+
+            // act
+            string result = sut.Operation();
+
+            // assert
+            Assert.Equal("Hello from ComponentA", result);
+        }
+    }
+}
